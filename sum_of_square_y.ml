fb5a507cc50aeecb39c874e804abf61a1d546f5b
youku_s <thebottomofwaterofhali@gmail.com>
2012-02-05 17:36:57 +0900

    misc

--- sum_of_square_y.ml
+++ sum_of_square_y.ml
@@ -1,4 +1,4 @@
 let sum_of_square num = 
   let rec compute n sum = 
-    if n <= 0 then sum else compute (n - 1) (sum + n * n)
+    if n = 0 then sum else compute (n - 1) (sum + n * n)
   in compute (abs num) 0
