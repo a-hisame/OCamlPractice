270f4fed7a6d69d5616cc2a49302c94d73c6087d
youku_s <thebottomofwaterofhali@gmail.com>
2012-02-05 15:42:00 +0900

    crain totoise calculus with test

--- crain_totoise_y.ml
+++ crain_totoise_y.ml
@@ -4,9 +4,9 @@ let totoise_num legs num = (legs - 2 * num) / 2
 let crain_totoise legs num = let totoise = totoise_num legs num
 	in (totoise, num - totoise)
 
-let test1 = crain_totoise 12 4
+let test1 = crain_totoise 12 4 = (2, 2)
 
-let test2 = crain_totoise 8 2
+let test2 = crain_totoise 8 2 = (2, 0)
 
-let test3 = crain_totoise 4 2
+let test3 = crain_totoise 4 2 = (0, 2)
 
