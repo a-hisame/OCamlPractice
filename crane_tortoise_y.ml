eb87e38a72beb5c79b6e83a27614fce1b61a628c
youku_s <thebottomofwaterofhali@gmail.com>
2012-02-05 16:11:48 +0900

    revise contents

--- crane_tortoise_y.ml
+++ crane_tortoise_y.ml
@@ -1,13 +1,13 @@
 (* youku_s *)
-let totoise_num legs num = (legs - 2 * num) / 2
+let tortoise_num legs num = (legs - 2 * num) / 2
 
-let crain_totoise legs num = let totoise = totoise_num legs num
-	in (totoise, num - totoise)
+let crane_tortoise legs num = let tortoise = tortoise_num legs num
+	in (tortoise, num - tortoise)
 
-let test1 = crain_totoise 12 4 = (2, 2)
+let test1 = crane_tortoise 12 4 = (2, 2)
 
-let test2 = crain_totoise 8 2 = (2, 0)
+let test2 = crane_tortoise 8 2 = (2, 0)
 
-let test3 = crain_totoise 4 2 = (0, 2)
+let test3 = crane_tortoise 4 2 = (0, 2)
 
-let test4 = crain_totoise 0 0 = (0, 0)
+let test4 = crane_tortoise 0 0 = (0, 0)
